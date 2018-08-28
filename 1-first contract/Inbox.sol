bd5f29794a8331d6b87998cca8c8a9f660397d5c
Aman Ali <pamanalionline@gmail.com>
2018-08-28 13:24:45 -0400

    Update Inbox.sol

--- 1-first contract/Inbox.sol
+++ 1-first contract/Inbox.sol
@@ -1,3 +1,15 @@
 pragma solidity ^0.4.17;
 
+contract Inbox{
+  string public message; // its a message accessable by any account
 
+  function Inbox(string initialMessage) public {
+    message = initialMessage;   
+  }
+  function setMessage(string newMessage) public {
+    message = newMessage;
+  }
+  function getMessage() public view returns (string) {
+    return message;
+  }
+}
